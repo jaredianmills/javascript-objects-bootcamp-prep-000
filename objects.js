e478428b8484585f19a0863829c911072e405386
jaredianmills <jaredianmills@gmail.com>
2018-04-16 18:31:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,6 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return obj;
 }
 
-function updateObjectWithKeyAndValue(obj, key, value)
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value})
+}
