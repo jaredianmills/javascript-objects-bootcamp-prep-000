62e528b29eaa4fae5f1f70bc85374e209664226e
jaredianmills <jaredianmills@gmail.com>
2018-04-16 18:53:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Radiohead: 'Paranoid Android', ["Aphex Twin"]: "Xtal", Autechre: "Pen Expers"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var updatedPlaylist = Object.assign({}, playlist, artistName, songTitle);
-  return updatedPlaylist;
+  playlist[artistName] = songTitle;
+  return playlist
 }
