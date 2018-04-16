5732a71aa3d6fb92a0b5ee1c9a781ae152f3ac6c
jaredianmills <jaredianmills@gmail.com>
2018-04-16 18:30:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,4 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return obj;
 }
 
-const recipe = {eggs: 3};
-
-destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
-
-console.log(recipe)
+function updateObjectWithKeyAndValue(obj, key, value)
